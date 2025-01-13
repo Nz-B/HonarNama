271bb58f60a92bf29e92a36c7725df4ed9e573e8
Nazanin <nazaninbakhshi386@gmail.com>
2025-01-13 19:18:39 +0330

    Add the papular products

--- index.php
+++ index.php
@@ -39,6 +39,44 @@
   </div>
 </div>
 
+<div class="product-slider-container">
+  <h2 class="product-title">محبوب ترین آثار</h2>
+  <div class="product-slider">
+    <div class="product-slide">
+      <img src="http://localhost/WordpressWorkshop/wordpress/wp-content/uploads/2024/12/1729428713981-1.png" alt="عنوان پست">
+      <h1 id="painter-name"><a href="http://localhost/WordpressWorkshop/wordpress/2024/12/24/محبوب-ترین-اثر-4/">پریسا شجاع‌زاده</a></h1>
+      <h1>پاستل روغنی روی مقوا</h1>
+      <h2 class="width">40⨯29 سانتی متر</h2>
+      <hr>
+      <h2 class="mony">قیمت</h2>
+    </div>
+    <div class="product-slide">
+      <img src="http://localhost/WordpressWorkshop/wordpress/wp-content/uploads/2024/12/1722080820640.png" alt="عنوان پست">
+      <h1 id="painter-name"><a href="http://localhost/WordpressWorkshop/wordpress/2024/12/24/picture1/">فرینو اطلسی</a></h1>
+      <h1>کلاژ روی مقوا</h1>
+      <h2 class="width">35⨯24 سانتی متر</h2>
+      <hr>
+      <h2 class="mony">قیمت</h2>
+    </div>
+    <div class="product-slide">
+      <img src="http://localhost/WordpressWorkshop/wordpress/wp-content/uploads/2024/12/txwlusfloy.png" alt="عنوان پست">
+      <h1 id="painter-name"><a href="http://localhost/WordpressWorkshop/wordpress/2024/12/24/محبوب-ترین-اثر3/">پریسا شجاع‌زاده</a></h1>
+      <h1>پاستل روغنی روی مقوا</h1>
+      <h2 class="width">51⨯60 سانتی متر</h2>
+      <hr>
+      <h2 class="mony">قیمت</h2>
+    </div>
+    <div class="product-slide">
+      <img src="http://localhost/WordpressWorkshop/wordpress/wp-content/uploads/2024/12/artwork_035c60f6-1366-447c-9556-42dd9a9ac96d.webp" alt="عنوان پست">
+      <h1 id="painter-name"><a href="http://localhost/WordpressWorkshop/wordpress/2024/12/23/hello-world/">مهدی راحمی</a></h1>
+      <h1>اکریلیک</h1>
+      <h2 class="width">80⨯100 سانتی متر</h2>
+      <hr>
+      <h2 class="mony">قیمت</h2>
+    </div>
+  </div>
+</div>
+
 
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/script.js"></script>
   
